750d87083c4537e130d747b0dba09c586c01704b
Jeff <jbridges1119@gmail.com>
2022-10-11 10:57:25 -0600

    Change promise all

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -26,6 +26,18 @@ export default function useApplicationData(props) {
         interviewers: all[2].data,
       }));
     });
+    Promise.all([
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    })
   }, []);
 
   //Updates spots for specific day and returns all updates days state
